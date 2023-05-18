c238467d2d46e48b84428edb77ad4426153b2db3
IoanRsX <133961591+IoanRsX@users.noreply.github.com>
2023-05-18 10:42:35 +0100

    Meeting room acc .csv export
    This is a powershell script to export meeting room accounts to a .csv file

---
+++ Meeting room list.ps1
@@ -0,0 +1,28 @@
+﻿# Install and import the Exchange Online PowerShell module if you don't have it installed/imported already
+Install-Module -Name ExchangeOnlineManagement -Force
+Import-Module ExchangeOnlineManagement
+
+# Connect to Exchange Online - make sure you have activated HelpDesk Admin role / Exchange Admin in AAD
+Connect-ExchangeOnline
+
+# Retrieve all meeting rooms
+$meetingRooms = Get-ExoMailbox -RecipientTypeDetails RoomMailbox -ResultSize Unlimited
+
+# Create an array to store the room information
+$roomList = @()
+
+# Iterate through each meeting room
+foreach ($room in $meetingRooms) {
+    $roomInfo = [PSCustomObject]@{
+        DisplayName = $room.DisplayName
+        UPN = $room.UserPrincipalName
+            }
+
+    $roomList += $roomInfo
+}
+
+# Export the room list to a CSV file
+$roomList | Export-Csv -Path "c:\whatever\MeetingRooms.csv" -NoTypeInformation
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline
